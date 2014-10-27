0937b86728de752548eb68ae42097c039d1fc97f
Michael P. Gerlek <mpg@flaxen.com>
2014-10-27 14:45:51 -0400

    initial server

---
+++ server/bin/server.dart
@@ -0,0 +1,22 @@
+import 'package:start/start.dart';
+
+void main() {
+  start(host: "localhost", port: 8001).then((Server app) {
+
+    app.static('/Users/mgerlek/work/web');
+
+    /*
+    app.get('/hello/:name.:lastname?').listen((request) {
+      request.response
+        .header('Content-Type', 'text/html; charset=UTF-8')
+        .send('Hello, ${request.param('name')} ${request.param('lastname')}');
+    });
+
+    app.ws('/socket').listen((socket) {
+      socket.on('ping').listen((data) => socket.send('pong'));
+      socket.on('pong').listen((data) => socket.close(1000, 'requested'));
+    });
+    */
+
+  });
+}
