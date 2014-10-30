b96b478a6e05e7ada013024465ae6cdbe62de857
Michael P. Gerlek <mpg@flaxen.com>
2014-10-30 09:39:32 -0400

    setup of Polymer elements

---
+++ viewer/web/main.dart
@@ -0,0 +1,21 @@
+import 'dart:html';
+import 'dart:js';
+import 'dart:core';
+import 'package:polymer/polymer.dart';
+import 'package:core_elements/core_overlay.dart';
+
+void main()
+{
+  initPolymer().run(() {
+    
+    // Code that doesn't need to wait.
+
+    
+    Polymer.onReady.then((_) {
+      // Code that executes after elements have been upgraded.
+      
+
+    });
+  });
+}
+
