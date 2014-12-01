ed8c99b34e82b77f2218534dded0e4448e81c208
Michael P. Gerlek <mpg@flaxen.com>
2014-12-01 15:33:59 -0500

    serving point, with CORS

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -3,17 +3,66 @@ import 'package:watcher/watcher.dart';
 import 'proxy.dart';
 import 'dart:io';
 
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+//import 'package:shelf_route/shelf_route.dart';
+import 'package:shelf_route/shelf_route.dart' as r;
+import 'package:shelf_exception_response/exception_response.dart';
+import 'dart:convert';
 
 DirectoryWatcher watcher;
 
 void main() {
-    String srcDir = "/Users/mgerlek/work/data";
 
+    var router = r.router()
+            ..get('/', (_) => new Response.notFound(null), middleware: logRequests())
+            ..get('/points/{+file}', getPoints, middleware: logRequests())
+            ..get('/file/{+file}', getFile, middleware: logRequests());
+
+    var handler =
+            const Pipeline().addMiddleware(logRequests()).addMiddleware(exceptionResponse()).addHandler(router.handler);
+
+    r.printRoutes(router);
+
+    io.serve(handler, 'localhost', 12345).then((server) {
+        print('Serving at http://${server.address.host}:${server.port}');
+    });
+}
+
+Response getPoints(dynamic request) {
+    var a = "Content-Type";
+    var b = "application/json";
+    var m = {
+        a: b,
+        "Access-Control-Allow-Origin": "*, ",
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    };
+    //var s = "Contents of ${r.getPathParameter(request, 'file')}";
+    var ms = [[0, 0, 0], [1, 1, 1], [2, 2, 2], [4, 4, 4], [16, 16, 16], [64, 64, 64]];
+    var s = JSON.encode(ms);
+    return new Response.ok(s, headers: m);
 }
 
 
-void runWatcher(String srcDir)
-{
+Response getFile(dynamic request) {
+    var a = "Content-Type";
+    var b = "application/json";
+    var m = {
+        a: b
+    };
+    //var s = "Contents of ${r.getPathParameter(request, 'file')}";
+    var ms = {
+        "a": "alpha",
+        "b": "beta"
+    };
+    var s = JSON.encode(ms);
+    return new Response.ok(s, headers: m);
+}
+
+
+
+void runWatcher(String srcDir) {
     final String testfile = srcDir + "/foo";
 
     if (FileSystemEntity.isFileSync(testfile)) {
