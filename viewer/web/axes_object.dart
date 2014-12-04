5510371bdbb2c70c91315e2e97c9f78d8709f82a
Michael P. Gerlek <mpg@flaxen.com>
2014-12-04 08:28:15 -0500

    namespace the Three work

--- viewer/web/axes_object.dart
+++ viewer/web/axes_object.dart
@@ -3,48 +3,48 @@ library axes_object;
 import 'dart:core';
 import 'dart:math' as Math;
 import 'package:vector_math/vector_math.dart';
-import 'package:three/three.dart';
+import 'package:three/three.dart' as three;
 
 // taken from three/extra/helpers/axis_helper.dart
-class AxesObject extends Object3D {
+class AxesObject extends three.Object3D {
     AxesObject() : super() {
 
-        var lineGeometry = new Geometry();
+        var lineGeometry = new three.Geometry();
         lineGeometry.vertices.add(new Vector3.zero());
         lineGeometry.vertices.add(new Vector3(0.0, 100.0, 0.0));
 
         // radius top, radius bottom, height, segments-radius, segments-height
-        var coneGeometry = new CylinderGeometry(0.0, 3.0, 10.0, 8, 1);
+        var coneGeometry = new three.CylinderGeometry(0.0, 3.0, 10.0, 8, 1);
 
         var line, cone;
 
         // x
 
-        line = new Line(lineGeometry, new LineBasicMaterial(color: 0xff0000));
+        line = new three.Line(lineGeometry, new three.LineBasicMaterial(color: 0xff0000));
         line.rotation.z = -Math.PI / 2.0;
         this.add(line);
 
-        cone = new Mesh(coneGeometry, new MeshBasicMaterial(color: 0xff0000));
+        cone = new three.Mesh(coneGeometry, new three.MeshBasicMaterial(color: 0xff0000));
         cone.position.x = 100.0;
         cone.rotation.z = -Math.PI / 2.0;
         this.add(cone);
 
         // y
 
-        line = new Line(lineGeometry, new LineBasicMaterial(color: 0x00ff00));
+        line = new three.Line(lineGeometry, new three.LineBasicMaterial(color: 0x00ff00));
         this.add(line);
 
-        cone = new Mesh(coneGeometry, new MeshBasicMaterial(color: 0x00ff00));
+        cone = new three.Mesh(coneGeometry, new three.MeshBasicMaterial(color: 0x00ff00));
         cone.position.y = 100.0;
         this.add(cone);
 
         // z
 
-        line = new Line(lineGeometry, new LineBasicMaterial(color: 0x0000ff));
+        line = new three.Line(lineGeometry, new three.LineBasicMaterial(color: 0x0000ff));
         line.rotation.x = Math.PI / 2.0;
         this.add(line);
 
-        cone = new Mesh(coneGeometry, new MeshBasicMaterial(color: 0x0000ff));
+        cone = new three.Mesh(coneGeometry, new three.MeshBasicMaterial(color: 0x0000ff));
         cone.position.z = 100.0;
         cone.rotation.x = Math.PI / 2.0;
         this.add(cone);
