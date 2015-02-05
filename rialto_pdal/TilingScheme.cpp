990d4e8498b74fcca19c2c99f6e271e6ffe8685b
Michael P. Gerlek <mpg@flaxen.com>
2015-02-05 18:35:55 -0500

    initial pass

--- rialto_pdal/TilingScheme.cpp
+++ rialto_pdal/TilingScheme.cpp
@@ -2,10 +2,12 @@
 // This file may only be used under the MIT-style
 // license found in the accompanying LICENSE.txt file.
 
+#include "TilingScheme.hpp"
+
 
 TilingScheme::TilingScheme(Rectangle& rectangle,
-                           boost::uint32 numberOfLevelZeroTilesX,       // 2
-                           boost::uint32 numberOfLevelZeroTilesY)       // 1
+                           boost::uint32_t numberOfLevelZeroTilesX,       // 2
+                           boost::uint32_t numberOfLevelZeroTilesY)       // 1
 {
     _rectangle = rectangle;
     _numberOfLevelZeroTilesX = numberOfLevelZeroTilesX;
@@ -19,110 +21,105 @@ const Rectangle& TilingScheme::getRectangle() const
 }
 
 
-int TilingScheme::getNumberOfXTilesAtLevel(boost::uint32 level)
+boost::uint32_t TilingScheme::getNumberOfXTilesAtLevel(boost::uint32_t level) const
 {
     return _numberOfLevelZeroTilesX << level;
 };
 
 
-int TilingScheme::getNumberOfYTilesAtLevel(boost::uint32 level)
+boost::uint32_t TilingScheme::getNumberOfYTilesAtLevel(boost::uint32_t level) const
 {
     return _numberOfLevelZeroTilesY << level;
 }
 
 
-GeographicTilingScheme::rectangleToNativeRectangle(const Rectangle& rectangle) const
-{    
-    var west = CesiumMath.toDegrees(rectangle.west);
-    var south = CesiumMath.toDegrees(rectangle.south);
-    var east = CesiumMath.toDegrees(rectangle.east);
-    var north = CesiumMath.toDegrees(rectangle.north);
-    
-    if (!defined(result)) {
-        return new Rectangle(west, south, east, north);
-    }
-    
-    result.west = west;
-    result.south = south;
-    result.east = east;
-    result.north = north;
+Rectangle TilingScheme::rectangleToNativeRectangle(const Rectangle& rectangle) const
+{
+    boost::uint32_t west = toDegrees(rectangle.west);
+    boost::uint32_t south = toDegrees(rectangle.south);
+    boost::uint32_t east = toDegrees(rectangle.east);
+    boost::uint32_t north = toDegrees(rectangle.north);
+
+    Rectangle result(north, south, east, west);
     return result;
 }
 
 
-Rectangle TilingScheme::tileXYToNativeRectangle(boost:uint32 x, boost:uint32 y, boost:uint32 level) const
+Rectangle TilingScheme::tileXYToNativeRectangle(boost::uint32_t x, boost::uint32_t y, boost::uint32_t level) const
 {
-    var rectangleRadians = this.tileXYToRectangle(x, y, level, result);
-    rectangleRadians.west = CesiumMath.toDegrees(rectangleRadians.west);
-    rectangleRadians.south = CesiumMath.toDegrees(rectangleRadians.south);
-    rectangleRadians.east = CesiumMath.toDegrees(rectangleRadians.east);
-    rectangleRadians.north = CesiumMath.toDegrees(rectangleRadians.north);
+    Rectangle rectangleRadians = tileXYToRectangle(x, y, level);
+    rectangleRadians.west = toDegrees(rectangleRadians.west);
+    rectangleRadians.south = toDegrees(rectangleRadians.south);
+    rectangleRadians.east = toDegrees(rectangleRadians.east);
+    rectangleRadians.north = toDegrees(rectangleRadians.north);
     return rectangleRadians;
 }
 
- 
-Rectangle TilingScheme::tileXYToRectangle(boost:uint32 x, boost:uint32 y, boost:uint32 level) const
-    var rectangle = this._rectangle;
-    
-    var xTiles = this.getNumberOfXTilesAtLevel(level);
-    var yTiles = this.getNumberOfYTilesAtLevel(level);
-    
-    var xTileWidth = rectangle.width / xTiles;
-    var west = x * xTileWidth + rectangle.west;
-    var east = (x + 1) * xTileWidth + rectangle.west;
-    
-    var yTileHeight = rectangle.height / yTiles;
-    var north = rectangle.north - y * yTileHeight;
-    var south = rectangle.north - (y + 1) * yTileHeight;
-    
-    if (!defined(result)) {
-        result = new Rectangle(west, south, east, north);
-    }
-    
-    result.west = west;
-    result.south = south;
-    result.east = east;
-    result.north = north;
+
+Rectangle TilingScheme::tileXYToRectangle(boost::uint32_t x, boost::uint32_t y, boost::uint32_t level) const
+{
+    boost::uint32_t xTiles = getNumberOfXTilesAtLevel(level);
+    boost::uint32_t yTiles = getNumberOfYTilesAtLevel(level);
+
+    boost::uint32_t xTileWidth = _rectangle.width() / xTiles;
+    boost::uint32_t west = x * xTileWidth + _rectangle.west;
+    boost::uint32_t east = (x + 1) * xTileWidth + _rectangle.west;
+
+    boost::uint32_t yTileHeight = _rectangle.height() / yTiles;
+    boost::uint32_t north = _rectangle.north - y * yTileHeight;
+    boost::uint32_t south = _rectangle.north - (y + 1) * yTileHeight;
+
+    Rectangle result(west, south, east, north);
     return result;
 }
 
  // position in cartographic degrees
- void TilingScheme::positionToTileXY(double xPosition, double yPosition,
-                                     boost::uint32& level,
-                                     boost::uint32& xResult, boost::uint32& yResult)
+ bool TilingScheme::positionToTileXY(double xPosition, double yPosition,
+                                     boost::uint32_t& level,
+                                     boost::uint32_t& xResult, boost::uint32_t& yResult) const
 {
-    var rectangle = this._rectangle;
-    if (!Rectangle.contains(rectangle, position)) {
+    static const double PI = boost::math::constants::pi<double>();
+
+    if (!_rectangle.contains(xPosition, yPosition)) {
         // outside the bounds of the tiling scheme
-        return undefined;
+        return false;
     }
 
-    var xTiles = this.getNumberOfXTilesAtLevel(level);
-    var yTiles = this.getNumberOfYTilesAtLevel(level);
+    boost::uint32_t xTiles = getNumberOfXTilesAtLevel(level);
+    boost::uint32_t yTiles = getNumberOfYTilesAtLevel(level);
 
-    var xTileWidth = rectangle.width / xTiles;
-    var yTileHeight = rectangle.height / yTiles;
+    boost::uint32_t xTileWidth = _rectangle.width() / xTiles;
+    boost::uint32_t yTileHeight = _rectangle.height() / yTiles;
 
-    var longitude = position.longitude;
-    if (rectangle.east < rectangle.west) {
-        longitude += CesiumMath.TWO_PI;
+    if (_rectangle.east < _rectangle.west) {
+        xPosition += PI * 2.0;
     }
 
-    var xTileCoordinate = (longitude - rectangle.west) / xTileWidth | 0;
+    boost::uint32_t xTileCoordinate = (xPosition - _rectangle.west) / xTileWidth; // TODO: trunc
     if (xTileCoordinate >= xTiles) {
         xTileCoordinate = xTiles - 1;
     }
 
-    var yTileCoordinate = (rectangle.north - position.latitude) / yTileHeight | 0;
+    boost::uint32_t yTileCoordinate = (_rectangle.north - yPosition) / yTileHeight; // TODO: trunc
     if (yTileCoordinate >= yTiles) {
         yTileCoordinate = yTiles - 1;
     }
 
-    if (!defined(result)) {
-        return new Cartesian2(xTileCoordinate, yTileCoordinate);
-    }
+    xResult = xTileCoordinate;
+    yResult = yTileCoordinate;
 
-    result.x = xTileCoordinate;
-    result.y = yTileCoordinate;
-    return result;
+    return true;
 }
+
+static const double PI = boost::math::constants::pi<double>();
+
+double TilingScheme::toDegrees(double radians)
+{
+    return radians * (180.0/PI);
+};
+
+
+double TilingScheme::toRadians(double degrees)
+{
+    return degrees * (PI/180.0);
+};
