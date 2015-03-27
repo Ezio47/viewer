580cd9bb650bb69c263ee839a105a38ef465fc0d
Ben Hosmer <ben.hosmer@gmail.com>
2015-03-27 13:31:00 -0400

    Use docker-compose for building, running the stack

--- docker/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-docker run -d -p 80:80 --name='rialto.dev' -restart=always radiantblue/tuple-rialto:latest
-Put quickstart shell commands here that start the tuple viewer and
-tuple wps containers, and link them with proper networking etc.
-
