4e43abf403f6ca56242e6b1ff723a70e9469a1a4
Michael P. Gerlek <mpg@flaxen.com>
2015-07-10 09:47:03 -0400

    added <pre> back to info dialog

--- web/frontend_private/ui_components/layer_info_dialog.dart
+++ web/frontend_private/ui_components/layer_info_dialog.dart
@@ -5,70 +5,77 @@
 part of rialto.frontend.private;
 
 class LayerInfoDialog extends DialogVM {
-  PreElement eee;
+  PreElement _preElement;
 
-  LayerInfoDialog(RialtoFrontend frontend, String id)
-      : super(frontend, id, hasCancelButton: false) {
-    eee = querySelector("#infoDialog_body");
+  LayerInfoDialog(RialtoFrontend frontend, String id) : super(frontend, id, hasCancelButton: false) {
+    _preElement = querySelector("#infoDialog_body");
   }
 
   @override
   void _show() {
-    eee.text = "dd";
+    String str = "";
 
     for (var layer in _backend.layerManager.layers) {
-//      _listbox.add(layer);
+      str += _getLayerInfo(layer);
     }
+
+    _preElement.text = str;
+  }
+
+  String _getLayerInfo(Layer layer) {
+    String str = "";
+
+    str += "Source\n";
+    str += "======\n";
+
+    str += "  Name: ${layer.name}\n";
+    str += "  Type: ${layer.type}\n";
+    str += "  Description: ${layer.description}\n";
+
+    if (layer.url != null) {
+      final url = layer.url.toString();
+      str += "  URL: $url";
+      if (layer.proxy != null) {
+        final proxy = layer.proxy.toString();
+        str += "  Proxy: $proxy\n";
+      }
+    }
+
+    if (layer.bbox != null) {
+      str += "\n";
+      str += "Bounding Box\n";
+      str += "============\n";
+
+      final precision = _backend.displayPrecision;
+
+      final minx = layer.bbox.minimum.longitude.toStringAsFixed(precision);
+      final miny = layer.bbox.minimum.latitude.toStringAsFixed(precision);
+      final minz = layer.bbox.minimum.height.toStringAsFixed(precision);
+      final maxx = layer.bbox.maximum.longitude.toStringAsFixed(precision);
+      final maxy = layer.bbox.maximum.latitude.toStringAsFixed(precision);
+      final maxz = layer.bbox.maximum.height.toStringAsFixed(precision);
+
+      str += "  Min X: $minx\n";
+      str += "  Min Y: $miny\n";
+      str += "  Min Z: $minz\n";
+      str += "  Max X: $maxx\n";
+      str += "  Max Y: $maxy\n";
+      str += "  Max Z: $maxz\n";
+    }
+
+    if (layer is PointCloudLayer) {
+      str += "\n";
+      str += "Point Cloud\n";
+      str += "===========\n";
+
+      str += "  Nuymber of points: ${layer.numPoints.toString()}\n";
+    }
+
+    str += "\n\n";
+
+    return str;
   }
 
   @override
   void _hide() {}
-
-  /*
-    final name = layer.name;
-    final type = layer.type;
-    final description = layer.description;
-
-    String url, proxy;
-    if (layer.url != null) {
-      url = layer.url.toString();
-      if (layer.proxy != null) {
-        proxy = layer.proxy.toString();
-      }
-    }
-
-    String minx, miny, minz;
-    String maxx, maxy, maxz;
-    if (layer.bbox != null) {
-      final precision = _backend.displayPrecision;
-
-      minx = layer.bbox.minimum.longitude.toStringAsFixed(precision);
-      miny = layer.bbox.minimum.latitude.toStringAsFixed(precision);
-      minz = layer.bbox.minimum.height.toStringAsFixed(precision);
-      maxx = layer.bbox.maximum.longitude.toStringAsFixed(precision);
-      maxy = layer.bbox.maximum.latitude.toStringAsFixed(precision);
-      maxz = layer.bbox.maximum.height.toStringAsFixed(precision);
-    }
-
-    String numPoints;
-    if (layer is PointCloudLayer) {
-      numPoints = layer.numPoints.toString();
-    }
-
-    querySelector("#infoDialog_name").text = name;
-    querySelector("#infoDialog_type").text = type;
-    querySelector("#infoDialog_url").text = url;
-    querySelector("#infoDialog_proxy").text = proxy;
-    querySelector("#infoDialog_description").text = description;
-
-    querySelector("#infoDialog_numPoints").text = numPoints;
-
-    querySelector("#infoDialog_minX").text = minx;
-    querySelector("#infoDialog_minY").text = miny;
-    querySelector("#infoDialog_minZ").text = minz;
-
-    querySelector("#infoDialog_maxX").text = maxx;
-    querySelector("#infoDialog_maxY").text = maxy;
-    querySelector("#infoDialog_maxZ").text = maxz;
-  */
 }
